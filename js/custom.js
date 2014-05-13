4875b28d141fe8b0308995f8530d84109ab3dc71
Adi Eyal <adi@burgercom.co.za>
2014-05-13 20:20:11 +0200

    Updated event date for the countdown timer

--- js/custom.js
+++ js/custom.js
@@ -108,7 +108,7 @@ $(document).ready(function () {
 	
 	// Countdown //
 	$('#countdown').countdown({
-		until: new Date(2014, 6 - 1, 24), // new Date(year, mth - 1, day, hr, min, sec) - date/time to count down to 
+		until: new Date(2014, 6 - 1, 30), // new Date(year, mth - 1, day, hr, min, sec) - date/time to count down to 
 		// or numeric for seconds offset, or string for unit offset(s): 
 		// 'Y' years, 'O' months, 'W' weeks, 'D' days, 'H' hours, 'M' minutes, 'S' seconds 
 		// until: '-1m +1d', for demo
